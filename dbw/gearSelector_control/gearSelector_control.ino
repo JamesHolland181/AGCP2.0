1a8182c81c3d9027832127bd79749575d24d1230
James Holland <jamesholland181@gmail.com>
2020-11-01 22:07:20 -0600

    Migrating to version 2
    Version 1 code updated to version 2.
    
    - Integration of CAN shield
    - First iteration of communication protocol
    - Code is not fully functional at the moment

--- dbw/gearSelector_control/gearSelector_control.ino
+++ dbw/gearSelector_control/gearSelector_control.ino
@@ -10,28 +10,73 @@
 /* Outputs: Forward, Reverse, Neutral
 /*********************************************************************************************************************/
 
+///////
+//
+// CAN-Bus Preamble
+// 
+///////
 
-// NEEDS REVISION
+#include <mcp_can.h>
+#include <SPI.h>
+
+/*SAMD core*/
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+    #define SERIAL SerialUSB
+#else
+    #define SERIAL Serial
+#endif
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+unsigned char len = 0;
+unsigned char buf[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char broadcast[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int msg=0;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+////////
+//
+// Gear Selector Variables
+//
+////////
 
 int DirectionRelay = 3;
 int ForwardSwitch = 5;
 int ReverseSwitch = 4;
+int current_dir = 000;
 
-void setup(){
-    // define the function of the pins
-    pinMode(DirectionRelay, OUTPUT);    
-    pinMode(ForwardSwitch, INPUT);
-    pinMode(ReverseSwitch, INPUT);
+ ////////////////////
+// Helper functions //
+ ////////////////////
+
+// Receive CAN messages
+char can_msg_to_input(void){
+    len = 0;
+
+    CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+    unsigned long canId = CAN.getCanId();
+
+    SERIAL.println("-----------------------------");
+    SERIAL.print("Get data from ID: 0x");
+    SERIAL.println(canId, HEX);
     
-    Serial.begin(9600);    
+    for (int i = 0; i < len; i++) { // print the data
+        SERIAL.print(buf[i], HEX);
+        SERIAL.print("\t");
+    }
+    SERIAL.println();
+    
+    return buf; // append to input
 }
 
-
-// Inside this loop, blah blah blah shall occur
-void loop(){
+// input handler
+void input_handler(char input){  
     int F_Direction = digitalRead(ForwardSwitch);
     int R_Direction = digitalRead(ReverseSwitch);
-    
     
     while(F_Direction == HIGH || R_Direction == HIGH)
     {
@@ -42,41 +87,58 @@ void loop(){
         
         if(F_Direction == HIGH)
             {
-                digitalWrite(DirectionRelay, LOW);
+                digitalWrite(DirectionRelay, LOW); current_dir = 100;
             }
         else if(R_Direction == HIGH)
             {
-                digitalWrite(DirectionRelay,HIGH);
+                digitalWrite(DirectionRelay,HIGH); current_dir = 001;
             }
         else
             {
-                    analogWrite(Motor,0);
-                    digitalWrite(DirectionRelay,LOW);
-                break;
-            }
-            Serial.println(Pedal);
-            Pedal = analogRead(A0);
-        
-        if(Pedal < 80)
-            {
-                analogWrite(Motor,0);
-            }
-        else
-            {
-            int pwm = map(Pedal,80,885,0,255);
-            
-            if(R_Direction == HIGH)
-                {
-                    analogWrite(Motor,pwm-5); 
-                }
-            else
-                {
-                    analogWrite(Motor,pwm); 
-                }
-            }
-        
-    }    
+                current_dir = 000; break;
+            }      
+    }
+}    
+
+void setup(){
+    // define the function of the pins
+    pinMode(DirectionRelay, OUTPUT);    
+    pinMode(ForwardSwitch, INPUT);
+    pinMode(ReverseSwitch, INPUT);
     
-    input = "";
+    // CAN-BUS Setup
+    SERIAL.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+        SERIAL.println("CAN BUS Shield init fail");
+        SERIAL.println("Init CAN BUS Shield again");
+        delay(100);
+    }
+    SERIAL.println("CAN BUS Shield init ok!");
+}
+
+void loop(){
+  //Serial.println("in loop");
+    broadcast[5] = 0; broadcast[6] = 0; broadcast[7] = 0; // reset CAN broadcast
+  
+    // handle receiving inputs
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {         // check if data coming
+        input_handler(can_msg_to_input()); // convert can message to input  
+          
+        // formatting CAN broadcast
+        if(current_dir = 100){
+          broadcast[5] = 1;
+        }
+        else if(current_dir = 001){
+          broadcast[7] = 1;
+        }
+        else{
+          broadcast[5] = 0; broadcast[6] = 0; broadcast[7] = 0;
+        }
+    }
+
+    // push broadcast (id, ext, length, buffer)
+    CAN.sendMsgBuf(0x02, 0, 8, broadcast);
+    delay(3000);
 }
     
