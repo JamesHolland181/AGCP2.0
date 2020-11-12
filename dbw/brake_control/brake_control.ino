87d89b72558a6ccdeffb5e9b7d9010f42dd3994b
James Holland <jamesholland181@gmail.com>
2020-11-12 14:54:13 -0600

    Update brake_control.ino

--- dbw/brake_control/brake_control.ino
+++ dbw/brake_control/brake_control.ino
@@ -9,19 +9,44 @@
 /* Outputs: Apply brakes or Release brakes
 /*********************************************************************************************************************/
 
+// Brake Variables
 
-// NEEDS REVISION
+int engage[2] = {3,5}; // pin combination for engaging the brake --> IN1 HIGH, IN2 LOW
+int disengage[2] = {3,5}; // pin combination for disengaging the brake --> IN1 LOW, IN2 LOW
+int speed_control = 6; // control speed of actuator with PWM from D6
+int braking_percentage = 0;
+int pwm = 0; // 0-255, control speed of actuator/braking
+
+int potentiometer = A0; // for feedback
+int maxAnalogReading; // max val of pot
+int minAnalogReading; // min val of pot
+bool limit = false;
+String input = "";
 
 void setup(){
  
     Serial.begin(9600);    
+
+    pinMode(engage[0],OUTPUT);
+    pinMode(engage[1],OUTPUT);
+
+    pinMode(disengage[0],OUTPUT);
+    pinMode(disengage[1],OUTPUT);
+
+    pinMode(speed_control,OUTPUT);
+
+    pinMode(potentiometer, INPUT);
+
+    // find limits of potentiometer
+    maxAnalogReading = moveToLimit(1);
+    minAnalogReading = moveToLimit(-1);
 }
 
 void loop(){
     // handle receiving inputs
     while (Serial.available() > 0){
         char c = Serial.read();
-        input = += c;
+        input += c;
         delay(5);
     }
     
@@ -35,15 +60,31 @@ void loop(){
         Serial.println("Brake");
         input.remove(0,(input.indexOf(":")+1));
         Serial.println(input);
-        
+        pwm = map(input.toInt(),0,100,40,255); 
+
         // Apply brakes
-        if(input.toInt() == 1)
+        if(input.toInt() >= 1)
         {
         delay(500);
         Serial.println("on");
-        digitalWrite(BrakeOff,LOW);
-        delay(500);
-        digitalWrite(BrakeOn,HIGH);
+        
+        // implement feedback
+        while(limit == false){
+          digitalWrite(speed_control,pwm);
+          digitalWrite(engage[0],HIGH);
+
+          // break from loop if limits have been reached
+          if((A0 >= maxAnalogReading)or(A0 <= minAnalogReading)){ break;}
+
+          // break from loop if desired braking percentage reached
+          if((A0/(maxAnalogReading-minAnalogReading)) == braking_percentage){break;}
+        }
+
+        // power down
+        digitalWrite(speed_control,0);
+        digitalWrite(engage[0],LOW); 
+        digitalWrite(engage[1],LOW); 
+        
         }
         
         // Release brakes
@@ -51,15 +92,48 @@ void loop(){
         {
         delay(500);
         Serial.println("off");
-        digitalWrite(BrakeOn,LOW);
-        delay(500);
-        digitalWrite(BrakeOff,HIGH);
+         
+        while(limit == false){
+          digitalWrite(speed_control,pwm);
+          digitalWrite(disengage[0],HIGH);
+
+          if((A0 >= maxAnalogReading)or(A0 <= minAnalogReading)){ break;}
+        }
+
+        // power down 
+        digitalWrite(speed_control, 0);
+        digitalWrite(disengage[0],LOW); 
+        digitalWrite(disengage[1],LOW);
         }    
 
+        // implement potentiometer feedback to stop power supply
         delay(2000);
-        digitalWrite(BrakeOn,LOW);
-        digitalWrite(BrakeOff,LOW);
+        digitalWrite(disengage[0],LOW);
+        digitalWrite(disengage[1],LOW);
     }    
     
     input = "";
+}
+
+int moveToLimit(int Direction){
+  int prevReading=0;
+  int currReading=0;
+  do{
+    prevReading = currReading;
+    
+    if(Direction ==1){
+        digitalWrite(speed_control,pwm);
+        digitalWrite(engage[0],HIGH);
+        digitalWrite(engage[1],LOW);
+    }
+    else if(Direction == -1){
+        digitalWrite(speed_control,pwm);
+        digitalWrite(disengage[0],HIGH);
+        digitalWrite(disengage[1],LOW);
+    }
+
+    delay(200); //keep moving until analog reading remains the same for 200ms
+    currReading = analogRead(potentiometer);
+  }while(prevReading != currReading);
+  return currReading;
 }
